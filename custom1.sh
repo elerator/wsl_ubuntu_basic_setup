34da9b4bde4e653d731c18d865a179cbbd18e97c
elerator <43006409+elerator@users.noreply.github.com>
2020-04-28 21:00:34 +0200

    Update custom1.sh

--- custom1.sh
+++ custom1.sh
@@ -24,3 +24,10 @@ apt-get install qttools5-dev-tools -y
 
 echo 'alias tolight="sshpass -p YOUR_PASSWORD_HERE ssh -tt mgerstenberg@gate.ikw.uos.de ssh -tt twilight"' >> .bashrc
 echo 'export DISPLAY=:0' >> .bashrc
+
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
